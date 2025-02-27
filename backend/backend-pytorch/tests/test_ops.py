546796cdb0266c854468aa6b4c95ba3e21703ba0
Zhiyao Ma <zhiyao.ma.98@gmail.com>
2025-02-27 18:53:32 +0000

    Refactor attention kernel and rename variables

--- backend/backend-pytorch/tests/test_ops.py
+++ backend/backend-pytorch/tests/test_ops.py
@@ -10,6 +10,7 @@ if parent_dir not in sys.path:
 import torch
 from ops import *
 
+@torch.inference_mode()
 def test_rope_correctness():
     head_dim = 64
     batch_size = 100
@@ -30,6 +31,7 @@ def test_rope_correctness():
     assert torch.allclose(k_pos1, k_pos_triton, atol=1e-6)
 
 
+@torch.inference_mode()
 def test_rope_performance():
     head_dim = 64
     batch_size = 1000
@@ -66,3 +68,110 @@ def test_rope_performance():
 
     speedup = pytorch_elapsed / triton_elapsed - 1.0
     print('Speed up: %.1f%%' % (speedup * 100))
+
+
+
+
+@torch.inference_mode()
+def test_qkv_attention():
+    device = torch.device('cuda')
+
+    #### CREATE A DUMMY MODEL ####
+
+    # create a dummy model
+    num_heads = 9
+    head_dim = 32
+    hidden_size = head_dim * num_heads
+    num_key_value_heads = num_heads // 1
+
+    # create a rope cache
+    # rope_cache = create_rope_cache(8192, head_dim, torch.float32, device)
+
+    q_proj = nn.Linear(hidden_size, num_heads * head_dim, bias=False, device=device)
+    # k_proj = nn.Linear(hidden_size, num_key_value_heads * head_dim, bias=False, device=device)
+    # v_proj = nn.Linear(hidden_size, num_key_value_heads * head_dim, bias=False, device=device)
+
+    ###############################
+
+    #### CREATE A DUMMY KV CACHE ####
+    NUM_TOTAL_BLOCKS = 128
+    BLOCK_SIZE = 32
+
+    # create a dummy kv cache
+    kv_cache_table = torch.randn(NUM_TOTAL_BLOCKS, num_key_value_heads, BLOCK_SIZE * 2, head_dim, device=device)
+
+    ###############################
+
+    #### CREATE A DUMMY TASK BATCH ####
+    CHUNK_SIZE = 3
+    NUM_REQS = 5
+    tasks = []
+
+    for _ in range(NUM_REQS):
+        # pick random number between 1 and 10
+        num_blocks = 10  # random.randint(1, 8)
+
+        # select `num_total_blocks` amount of random block ids (does not need to be unique)
+        ctx_ids = random.choices(range(NUM_TOTAL_BLOCKS), k=num_blocks)
+
+        # create a random mask (num_total_blocks * block_size, block_size) using numpy
+        mask = np.random.choice([0, 1], size=(BLOCK_SIZE, num_blocks * BLOCK_SIZE), p=[0.1, 0.9])
+
+        # ensure the first block is always True (This is to ensure that the softmax is not NaN)
+        mask[:, 0] = 1
+
+        # create a full true mask
+        # mask = np.ones((BLOCK_SIZE, num_blocks * BLOCK_SIZE), dtype=np.bool_)
+
+        tasks.append((ctx_ids, mask))
+
+    inp_baseline = construct_input_baseline(tasks, num_tok_per_blk=BLOCK_SIZE, device=device)
+    inp = construct_input(tasks, num_blk_per_chunk=CHUNK_SIZE, num_tok_per_blk=BLOCK_SIZE, device=device)
+
+    # simulate the previous state
+    hidden_states = torch.randn(NUM_REQS, BLOCK_SIZE, hidden_size, device=device)
+
+    q = q_proj(hidden_states)
+    # k = k_proj(hidden_states)
+    # v = v_proj(hidden_states)
+
+    q = q.view(NUM_REQS, BLOCK_SIZE, num_heads, head_dim).transpose(1, 2)
+    # k = k.view(NUM_REQS, BLOCK_SIZE, num_key_value_heads, head_dim).transpose(1, 2)
+    # v = v.view(NUM_REQS, BLOCK_SIZE, num_key_value_heads, head_dim).transpose(1, 2)
+
+    # rope(rope_cache, q, batch.position_offsets)
+    # rope(rope_cache, k, batch.position_offsets)
+
+    # attention
+    y1 = qkv_attention_baseline(
+        q,
+        kv_cache_table,
+        inp_baseline['q_lut'],
+        inp_baseline['kv_lut'],
+        inp_baseline['mask']
+    )
+
+    # print(y1[0])
+
+    y2 = qkv_attention(
+        q,
+        kv_cache_table,
+        inp['q_lut'],
+        inp['kv_lut'],
+        inp['masks'],
+        inp['reduce_grps']
+    )
+
+    # print(y2[0])
+    # print('baseline:')
+    # print(y1.unsqueeze(0).unsqueeze(0))
+    #
+    # print('triton:')
+    # print(y2.unsqueeze(0).unsqueeze(0))
+
+    # shape
+    print('y1, y2', y1.shape, y2.shape)
+
+    print('y1, y2', torch.abs(y1 - y2).sum())
+
+    print('done')
