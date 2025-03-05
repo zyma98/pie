59bd6d2ff9d797dbce44b0f0fdf0c22764b6b3c2
In Gim <in.gim@yale.edu>
2025-03-05 12:27:16 -0800

    Add prefix sharing example

---
+++ example-apps/prefix-sharing/src/lib.rs
@@ -0,0 +1,45 @@
+use std::time::Instant;
+use symphony::RunSync;
+
+struct PrefixSharing;
+
+// create a default stream constant
+
+impl RunSync for PrefixSharing {
+    fn run() -> Result<(), String> {
+        let start = Instant::now();
+
+        let max_num_outputs = 128;
+
+        let mut ctx = symphony::Context::new();
+        ctx.fill("<|begin_of_text|>");
+        ctx.fill("<|start_header_id|>system<|end_header_id|>\n\nYou are a helpful, respectful and honest assistant.<|eot_id|>");
+
+        let mut ctx_sub1 = ctx.fork();
+        let mut ctx_sub2 = ctx.fork();
+
+        ctx_sub1.fill("<|start_header_id|>user<|end_header_id|>\n\nExplain the LLM decoding process ELI5.<|eot_id|>");
+        ctx_sub1.fill("<|start_header_id|>assistant<|end_header_id|>\n\n");
+
+        ctx_sub2.fill("<|start_header_id|>user<|end_header_id|>\n\nExplain the Espresso making process ELI5.<|eot_id|>");
+        ctx_sub2.fill("<|start_header_id|>assistant<|end_header_id|>\n\n");
+
+        let output_text1 = ctx_sub1.generate_until("<|eot_id|>", max_num_outputs);
+        let output_text2 = ctx_sub2.generate_until("<|eot_id|>", max_num_outputs);
+
+        println!(
+            "Output: {:?} (elapsed: {:?})",
+            output_text1,
+            start.elapsed()
+        );
+        println!(
+            "Output: {:?} (elapsed: {:?})",
+            output_text2,
+            start.elapsed()
+        );
+
+        Ok(())
+    }
+}
+
+symphony::main_sync!(PrefixSharing);
