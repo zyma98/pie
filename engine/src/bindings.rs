0823bf11cf92ed35f8faa31529378fdb3b2d055a
In Gim <in.gim@yale.edu>
2025-05-30 08:37:09 -0700

    Refactor symphony -> pie

--- engine/src/bindings.rs
+++ engine/src/bindings.rs
@@ -12,12 +12,12 @@ mod wit {
         async: true,
         with: {
             "wasi:io/poll": wasmtime_wasi::p2::bindings::io::poll,
-            "symphony:nbi/messaging/subscription": messaging::Subscription,
-            "symphony:nbi/messaging/receive-result": messaging::ReceiveResult,
-            "symphony:nbi/l4m/model": l4m::Model,
-            "symphony:nbi/l4m/tokenizer": l4m::Tokenizer,
-            "symphony:nbi/l4m/sample-top-k-result": l4m::SampleTopKResult,
-            "symphony:nbi/l4m/synchronization-result": l4m::SynchronizationResult,
+            "pie:nbi/messaging/subscription": messaging::Subscription,
+            "pie:nbi/messaging/receive-result": messaging::ReceiveResult,
+            "pie:nbi/l4m/model": l4m::Model,
+            "pie:nbi/l4m/tokenizer": l4m::Tokenizer,
+            "pie:nbi/l4m/sample-top-k-result": l4m::SampleTopKResult,
+            "pie:nbi/l4m/synchronization-result": l4m::SynchronizationResult,
         },
         trappable_imports: true,
     });
@@ -25,16 +25,16 @@ mod wit {
 
 pub fn add_to_linker<T>(linker: &mut wasmtime::component::Linker<T>) -> Result<(), wasmtime::Error>
 where
-    T: wit::symphony::nbi::l4m::Host
-        + wit::symphony::nbi::l4m_vision::Host
-        + wit::symphony::nbi::runtime::Host
-        + wit::symphony::nbi::ping::Host
-        + wit::symphony::nbi::messaging::Host,
+    T: wit::pie::nbi::l4m::Host
+        + wit::pie::nbi::l4m_vision::Host
+        + wit::pie::nbi::runtime::Host
+        + wit::pie::nbi::ping::Host
+        + wit::pie::nbi::messaging::Host,
 {
-    wit::symphony::nbi::l4m::add_to_linker(linker, |s| s)?;
-    wit::symphony::nbi::l4m_vision::add_to_linker(linker, |s| s)?;
-    wit::symphony::nbi::runtime::add_to_linker(linker, |s| s)?;
-    wit::symphony::nbi::ping::add_to_linker(linker, |s| s)?;
-    wit::symphony::nbi::messaging::add_to_linker(linker, |s| s)?;
+    wit::pie::nbi::l4m::add_to_linker(linker, |s| s)?;
+    wit::pie::nbi::l4m_vision::add_to_linker(linker, |s| s)?;
+    wit::pie::nbi::runtime::add_to_linker(linker, |s| s)?;
+    wit::pie::nbi::ping::add_to_linker(linker, |s| s)?;
+    wit::pie::nbi::messaging::add_to_linker(linker, |s| s)?;
     Ok(())
 }
