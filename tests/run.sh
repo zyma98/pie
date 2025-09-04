0a13c55fe3c5ef72e0db2946e567b89c98fbd565
Zhiyao Ma <zhiyao.ma.98@gmail.com>
2025-09-04 12:08:27 -0400

    Add CI test for type checking for selective backend code.

--- tests/run.sh
+++ tests/run.sh
@@ -12,4 +12,9 @@ echo "Running Pylint checks on Python backend..."
 ./tests/backend-python-pylint.sh
 echo "Done."
 
+# Run pyright checks.
+echo "Running Pyright checks on Python backend..."
+./tests/backend-python-pyright.sh
+echo "Done."
+
 echo "All checks passed."
