0d1245b0dc3f6287282e1353dc044387b7c94c83
In Gim <in.gim@yale.edu>
2025-09-08 08:09:07 -0700

    Add PYTHONPATH in pylint testing

--- tests/backend-python-pylint.sh
+++ tests/backend-python-pylint.sh
@@ -7,7 +7,7 @@ ROOT="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 # Run pylint check.
 # See `.pylintrc` for the configuration of the check.
 
-UV_NO_SYNC=1 UV_OFFLINE=1 uv \
+PYTHONPATH=${ROOT}/backend/backend-python UV_NO_SYNC=1 UV_OFFLINE=1 uv \
     --project ${ROOT}/backend/backend-python \
     run pylint --disable=R \
     ${ROOT}/backend/backend-python/config/*.py \
