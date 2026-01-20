273436e17b6d23d9fb3a04057c3667ba5206540d
Zhiyao Ma <zhiyao.ma.98@gmail.com>
2026-01-20 18:05:28 +0000

    TEMP

---
+++ load.sh
@@ -0,0 +1,6 @@
+pie-client load sdk/rust/inferlib/target/wasm32-wasip2/release/inferlib_brle.wasm
+pie-client load sdk/rust/inferlib/target/wasm32-wasip2/release/inferlib_chat.wasm
+pie-client load sdk/rust/inferlib/target/wasm32-wasip2/release/inferlib_environment.wasm
+pie-client load sdk/rust/inferlib/target/wasm32-wasip2/release/inferlib_model.wasm
+pie-client load sdk/rust/inferlib/target/wasm32-wasip2/release/inferlib_queue.wasm
+pie-client load sdk/rust/inferlib/target/wasm32-wasip2/release/inferlib_context.wasm -d inferlib_brle -d inferlib_chat -d inferlib_environment -d inferlib_model -d inferlib_queue
---
+++ run.sh
@@ -0,0 +1,3 @@
+pie-client submit --path std/text-completion-inferlib/target/wasm32-wasip2/release/text_completion_inferlib.wasm \
+    -d inferlib_chat -d inferlib_environment -d inferlib_context \
+    -- -p Hello
